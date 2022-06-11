830f4cb9656c82ae6e8f51a56749d99e60b9aa53
Victor Costa <87447640+victorcosta21@users.noreply.github.com>
2022-06-11 12:25:54 -0300

    Create tabuada.php

---
+++ tabuada.php
@@ -0,0 +1,15 @@
+<?php
+
+$num1 = 7;
+
+for($multi = 1; $multi <=10; $multi++){
+    $resultado = $num1 * $multi;
+    echo "$num1 x $multi = $resultado" . PHP_EOL;
+};<?php
+
+$num1 = 7;
+
+for($multi = 1; $multi <=10; $multi++){
+    $resultado = $num1 * $multi;
+    echo "$num1 x $multi = $resultado" . PHP_EOL;
+};
